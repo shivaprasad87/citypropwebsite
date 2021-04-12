1f6ff2d1211d0e9d361e0a0d82fa89bd51cace43
shiva prasad <shiva@secondsdigital.com>
2021-04-12 23:17:38 +0530

    thankyou page

--- application/views/thank-you.php
+++ application/views/thank-you.php
@@ -1,17 +1,96 @@
-<section class="fullwidth  margin-top-105 margin-bottom-0 padding-bottom-80 padding-top-95" data-background-color="#f7f7f7" style="background: rgb(255, 255, 255);">
+<!DOCTYPE html>
+<html lang="en">
 
-	<!-- Box Headline -->
-	<h3 class="headline-box">Thank You</h3>
+<head>
 
+	<title>City Prop </title>
+	<meta name="description" content="City Prop ">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- External CSS libraries -->
+	<link rel="stylesheet" type="text/css" href="<?=base_url('assets/')?>css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url('assets/')?>css/animate.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url('assets/')?>css/bootstrap-submenu.css">
+
+	<link rel="stylesheet" type="text/css" href="<?=base_url('assets/')?>css/bootstrap-select.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=base_url('assets/')?>css/magnific-popup.css">
+	<link rel="stylesheet" href="<?=base_url('assets/')?>css/leaflet.css" type="text/css">
+	<link rel="stylesheet" href="<?=base_url('assets/')?>css/map.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/flaticon/font/flaticon.css">
+	<link rel="stylesheet" type="text/css" href="fonts/linearicons/style.css">
+	<link rel="stylesheet" type="text/css"  href="<?=base_url('assets/')?>css/jquery.mCustomScrollbar.css">
+	<!-- <link rel="stylesheet" type="text/css"  href="<?=base_url('assets/')?>css/dropzone.css"> -->
+	<link rel="stylesheet" type="text/css"  href="<?=base_url('assets/')?>css/slick.css">
+
+	<!-- Custom stylesheet -->
+	<link rel="stylesheet" type="text/css" href="<?=base_url('assets/')?>css/style.css">
+	<link rel="stylesheet" type="text/css" id="style_sheet" href="<?=base_url('assets/')?>css/skins/default.css">
+
+	<!-- Favicon icon -->
+	<link rel="shortcut icon" href="<?=base_url('assets/')?>img/favicon.png" type="image/x-icon" >
+
+	<!-- Google fonts -->
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600,300,700">
+
+
+
+</head>
+<body>
+
+
+<!-- Coming soon start -->
+<div class="coming-soon coming-soon-bg">
 	<div class="container">
-		<div class="col-md-12">
-			<div class="testimonials-subtitle">For Showing Your Interest In City Prop</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="coming-soon-inner">
+					<a href="<?=base_url()?>"><img src="<?=base_url('assets/')?>img/logos/logo.png" alt="logo" style="width: 150px;height: 150px;background-color: white;padding: 5px;"></a>
+					<h1>Thank You</h1>
 
-			<p></p>
+					<div class="clearfix"></div>
+					<h6>For Showing Interest In Us<br>
+						For More Details<br>
+						Call Us on<br>
+						<a href="tel:+91<?= $social_links->mobile ?>" style="color: white">+91 <?= $social_links->mobile ?></a></h6>
+					<ul class="social-list clearfix">
+						<li><a href="<?= $social_links->facebook ?>" class="facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="<?= $social_links->twitter ?>" class="twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="<?= $social_links->instagram ?>" class="instagram-bg"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="<?= $social_links->linked_in ?>" class="linkedin-bg"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
 		</div>
-
-		<br>
-		
 	</div>
+</div>
+<!-- Coming soon end -->
 
-</section>
+
+
+<script src="<?=base_url('assets/')?>js/jquery-2.2.0.min.js"></script>
+<script src="<?=base_url('assets/')?>js/popper.min.js"></script>
+<script src="<?=base_url('assets/')?>js/bootstrap.min.js"></script>
+<script  src="<?=base_url('assets/')?>js/bootstrap-submenu.js"></script>
+<script  src="<?=base_url('assets/')?>js/rangeslider.js"></script>
+<script  src="<?=base_url('assets/')?>js/jquery.mb.YTPlayer.js"></script>
+<script  src="<?=base_url('assets/')?>js/bootstrap-select.min.js"></script>
+<script  src="<?=base_url('assets/')?>js/jquery.easing.1.3.js"></script>
+<script  src="<?=base_url('assets/')?>js/jquery.scrollUp.js"></script>
+<script  src="<?=base_url('assets/')?>js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script  src="<?=base_url('assets/')?>js/leaflet.js"></script>
+<script  src="<?=base_url('assets/')?>js/leaflet-providers.js"></script>
+<!-- <script  src="<?=base_url('assets/')?>js/leaflet.markercluster.js"></script>
+<script  src="<?=base_url('assets/')?>js/dropzone.js"></script> -->
+<script  src="<?=base_url('assets/')?>js/slick.min.js"></script>
+<!-- <script  src="<?=base_url('assets/')?>js/jquery.filterizr.js"></script> -->
+<script  src="<?=base_url('assets/')?>js/jquery.magnific-popup.min.js"></script>
+<script  src="<?=base_url('assets/')?>js/jquery.countdown.js"></script>
+<script  src="<?=base_url('assets/')?>js/maps.js"></script>
+<script  src="<?=base_url('assets/')?>js/app.js"></script>
+
+</body>
+
+</html>
